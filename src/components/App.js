daca77f6474a4c9f2c9d700d938f33c0b8368d78
Sasha92Shevchuk <108683139+Sasha92Shevchuk@users.noreply.github.com>
2023-02-17 19:59:03 +0200

    change class App to hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import Searchbar from './Searchbar';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -8,30 +8,23 @@ import Button from 'components/Button';
 import Loader from 'components/Loader';
 import { Box } from './App.styled';
 
-export class App extends Component {
-  state = {
-    searchQuery: '',
-    searchCards: [],
-    isLoading: false,
-    isVisible: false,
-    error: null,
-    page: 1,
-  };
+export function App() {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchCards, setSearchCards] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
+  const [error, setError] = useState(null);
+  const [page, setPage] = useState(1);
 
-  async componentDidUpdate(prevProps, prevState) {
-    const prevSearch = prevState.searchQuery;
-    const nextSearch = this.state.searchQuery;
-    const page = this.state.page;
-    // if (prevSearch === nextSearch) {
-    //   toast.warning('Please, enter new search query and try again.');
-    //   return;
-    // }
-
-    if (prevSearch !== nextSearch || prevState.page !== page) {
-      this.setState({ isLoading: true });
+  useEffect(() => {
+    if (!searchQuery) {
+      return;
+    }
+    const fetchData = async () => {
+      setIsLoading(true);
       try {
         const { hits, totalHits } = await instanceAPI.getImages(
-          nextSearch,
+          searchQuery,
           page
         );
         if (totalHits === 0) {
@@ -42,67 +35,54 @@ export class App extends Component {
         if (totalHits > 0 && page === 1) {
           toast.success(`Hooray! We found ${totalHits} images.`);
         }
-        if (
-          totalHits > 0 &&
-          totalHits === this.state.searchCards.length + hits.length
-        ) {
+        if (totalHits > 0 && totalHits === searchCards.length + hits.length) {
           toast.info(
             `We're sorry, but you've reached the end of search results.`
           );
         }
-        this.setState(prevState => ({
-          searchCards: [...prevState.searchCards, ...hits],
-          isVisible:
-            1 <
-            Math.ceil(
-              totalHits / (this.state.searchCards.length + hits.length)
-            ),
-        }));
+        setSearchCards(prevState => [...prevState, ...hits]);
+        setIsVisible(
+          1 < Math.ceil(totalHits / (searchCards.length + hits.length))
+        );
       } catch (error) {
-        this.setState({ error });
+        setError(error);
       } finally {
-        this.setState({ isLoading: false });
+        setIsLoading(false);
       }
-    }
-  }
+    };
+    fetchData();
+  }, [searchQuery, page]);
 
-  onFormSubmit = value => {
-    if (value === this.state.searchQuery) {
+  const onFormSubmit = value => {
+    if (value === searchQuery) {
       toast.warning('Please, enter new search query and try again.');
       return;
     } else {
-      this.setState({
-        searchQuery: value,
-        searchCards: [],
-        isLoading: false,
-        error: null,
-        page: 1,
-      });
+      setSearchQuery(value);
+      setSearchCards([]);
+      setIsLoading(false);
+      setError(null);
+      setPage(1);
     }
   };
-  LoadMore = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
+  const LoadMore = () => {
+    setPage(prevState => prevState + 1);
   };
 
-  render() {
-    const { searchCards, isVisible, isLoading, error } = this.state;
-    return (
-      <Box>
-        <Searchbar onSubmit={this.onFormSubmit} />
-        <ToastContainer autoClose={3000} />
-        {error && <h1>{error.message}</h1>}
-        {isLoading && <Loader />}
-        <ImageGallery searchCards={searchCards} />
-        {isVisible && (
-          <Button
-            loadMore={this.LoadMore}
-            isLoading={isLoading}
-            disabled={isLoading}
-          />
-        )}
-      </Box>
-    );
-  }
+  return (
+    <Box>
+      <Searchbar onSubmit={onFormSubmit} />
+      <ToastContainer autoClose={3000} />
+      {error && <h1>{error.message}</h1>}
+      {isLoading && <Loader />}
+      <ImageGallery searchCards={searchCards} />
+      {isVisible && (
+        <Button
+          loadMore={LoadMore}
+          isLoading={isLoading}
+          disabled={isLoading}
+        />
+      )}
+    </Box>
+  );
 }
